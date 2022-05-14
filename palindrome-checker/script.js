18ec429f6bd97dbc6e289cb26c9074c89409fc6b
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-05-15 00:47:01 +0630

    Palindrome checker done

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,7 +1,22 @@
-const textInput = document.querySelector(".form input");
+const textInput = document.querySelector(".form input"),
+  checkBtn = document.querySelector(".form button"),
+  infoText = document.querySelector(".info-text");
+let filterInput;
 
+checkBtn.addEventListener("click", () => {
+  let reverseInput = filterInput.split("").reverse().join("");
+  infoText.style.display = "block";
+  if (filterInput != reverseInput) {
+    return (infoText.innerHTML = `No, <span>'${textInput.value}'</span> isn't a palindrome!`);
+  }
+  return (infoText.innerHTML = `Yes, <span>'${textInput.value}'</span> is a palindrome!`);
+});
 textInput.addEventListener("keyup", () => {
   // remove spaces and all special chars
-  let filterInput = textInput.value.replace(/[^A-Z0-9]/gi, "");
-  console.log(filterInput);
+  filterInput = textInput.value.toLowerCase().replace(/[^A-Z0-9]/gi, "");
+  if (filterInput) {
+    return checkBtn.classList.add("active");
+  }
+  infoText.style.display = "none";
+  checkBtn.classList.remove("active");
 });
