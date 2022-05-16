7709b917eb706759cbdd1eede078cc8264ac1b11
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-05-16 23:32:11 +0630

    get-user-location done

--- get-user-location/script.js
+++ get-user-location/script.js
@@ -12,6 +12,7 @@ button.addEventListener("click", () => {
      *it takes three parameters (success,error,options). If everything is right, then
      *success callback function will get called, else, error callback function will get called. We don't need third parameter for this project.
      */
+    button.innerHTML = "Allow to detect your location.";
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
   } else {
     button.innerHTML = "You browser does not support geolocation";
@@ -19,10 +20,23 @@ button.addEventListener("click", () => {
 });
 
 function onSuccess(position) {
+  button.innerHTML = "Detecting your location...";
   let { latitude, longitude } = position.coords;
   fetch(
     `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${key}`
-  ).then((response) => response.json());
+  )
+    // parsing json data into JavaScript object and returning it and another then function receiving the object that is sent by the api
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      let allDetails = result.results[0].components;
+      let { county, country, postcode, city, road } = allDetails;
+      button.innerHTML = `${county} , ${postcode} , ${country}`;
+      console.table(allDetails);
+    })
+    .catch((error) => {
+      button.innerHTML = "Something went wrong :(";
+    });
 }
 
 function onError(error) {
